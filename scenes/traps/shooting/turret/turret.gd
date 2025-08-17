adc2b174d0a4dbc95f2ff1e236d7c33b81e25ea5
Overkane <overkane@mail.ru>
2025-08-17 21:03:57 +0500

    Fix turret shooting when there is no dummy

--- scenes/traps/shooting/turret/turret.gd
+++ scenes/traps/shooting/turret/turret.gd
@@ -7,16 +7,23 @@ extends Node2D
 @onready var gun_mount: Node2D = $GunMount
 @onready var projectile_spawn_point: Marker2D = $GunMount/ProjectileSpawnPoint
 
+var can_shoot := false
+
+
 func _ready() -> void:
-	shooting_cooldown_timer.timeout.connect(_shoot)
+	shooting_cooldown_timer.timeout.connect(func(): can_shoot = true)
 	shooting_cooldown_timer.wait_time = shooting_cooldown
 
 func _physics_process(delta: float) -> void:
 	if GameManager.dummy != null:
+		# TODO add lerp later
 		gun_mount.look_at(GameManager.dummy.get_hit_spot().global_position)
+		if can_shoot:
+			_shoot()
 
 
 func _shoot() -> void:
+	can_shoot = false
 	var projectile: Projectile = projectile_scene.instantiate()
 	add_child(projectile)
 	projectile.setup(projectile_spawn_point.global_position, Vector2.RIGHT.rotated(gun_mount.rotation))
